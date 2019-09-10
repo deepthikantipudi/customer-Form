9e1a0b808b7a683b580e03050ce6427d19715651
Sneha Salla <snehasalla1313@gmail.com>
2019-09-10 16:41:29 -0400

    integrating with backend

--- src/app/customer-form/customer-form.component.ts
+++ src/app/customer-form/customer-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import {  Router } from '@angular/router';
+import { CustomerService } from '../customer.service';
 
 @Component({
   selector: 'app-customer-form',
@@ -9,21 +10,33 @@ import {  Router } from '@angular/router';
 })
 export class CustomerFormComponent implements OnInit {
 @ViewChild('f', {static: false}) form: NgForm;
+message;
 
-formdata = {
-  FirstName: 'Sneha',
-  LastName: 'Salla',
-  Address1: '123 Main St',
-  Address2: '12',
-  City: 'Novi',
-  State: 'MI',
-  Country: 'USA',
-  ZipCode: '56003',
-  Phone: '5078976543',
-  EmailID: 'sneha.salla@gmail.com'
+// formdata = {
+//   FirstName: 'Sneha',
+//   LastName: 'Salla',
+//   Address1: '123 Main St',
+//   Address2: '12',
+//   City: 'Novi',
+//   State: 'MI',
+//   Country: 'USA',
+//   ZipCode: '56003',
+//   Phone: '5078976543',
+//   EmailID: 'sneha.salla@gmail.com'
 
-};
-  constructor(private router: Router) { }
+// };
+  constructor(private router: Router, private customerservice: CustomerService) {
+    this.customerservice.addCustomer(FormData).subscribe(
+      (res: { Message: String }) => {
+        this.message = res.Message;
+        this.router.navigate(["/success"]);
+      },
+      err => {
+        console.log("error", err);
+      });
+
+    }
+   
 
   ngOnInit() {
   }
--- src/app/customer.service.ts
+++ src/app/customer.service.ts
@@ -1,16 +1,43 @@
 import { Injectable } from '@angular/core';
 import { CustomerModel } from "./customer.model";
+import { Router } from "@angular/router";
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class CustomerService {
+  private headers = new HttpHeaders({ "Content-Type": "application/json" });
+message;
+  constructor(private http: HttpClient, private router: Router) {}
 
-  constructor() {}
+    // addCustomer(formData: CustomerModel) {
+    //   console.log("service", formData);
+    //   this.http.post("http://localhost:8080" );
+    // }
 
-    addCustomer(formData: CustomerModel) {
+
+    // addCustomer(formData) {
+    //   console.log("service", formData);
+    //   this.http
+    //     .post("http://localhost:8080/customers", formData, {
+    //       headers: this.headers
+    //     })
+    //     .subscribe(
+    //       (res: { Message: String }) => {
+    //         this.message = res.Message;
+    //         this.router.navigate(["/success"]);
+    //       },
+    //       err => {
+    //         console.log("error", err);
+    //       }
+    //     );
+    // }
+
+    
+    addCustomer(formData) {
       console.log("service", formData);
-      //this.http.post("");
+      this.http.post("http://localhost:8080/customers", formData, {headers: this.headers});
     }
 }
